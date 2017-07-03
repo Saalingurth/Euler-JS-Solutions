710ab81b992c115f0d047f9c1bd36a807086b366
Valery <saalingurth@gmail.com>
2017-07-03 18:35:12 +1200

    Update problem1.js

--- problem1.js
+++ problem1.js
@@ -1,6 +1,6 @@
 var result = 0;
 
-for (var number = 0; number < 1000; number += 1) {
+for (var number = 1; number < 1000; number += 1) {
   // we use "||" instead of multiple "if" statements because
   // they will add numbers that are multiples of both 3 and 5 twice
   // you could use "if... else if" statements
